a02e33ab1d3cf8b6fec56e19c3d1a8da02441f59
GuyBinyamin <46027624+GuyBinyamin@users.noreply.github.com>
2025-06-03 16:35:22 +0300

    Update connections.tf

--- clusters/connections.tf
+++ clusters/connections.tf
@@ -5,6 +5,6 @@ provider "aws" {
 provider "azurerm" {
   subscription_id = "e20c6fb8-c39f-44bd-9dae-eb43abbd1fea"
   client_id = "0ce58119-7825-4dd7-b3cb-1db04aa706c4"
-  client_secret = "61129948-ca5d-48c4-ba05-fedb097ac431"
+  client_secret = "vD8X~pRj7rT2qXJ3iM05_AaFvT9WzYdP4uc6bRmL"
   tenant_id = "c7c08208-4f4d-45f1-83cd-5e2f491ab786"
 }
